8234b936f4f85ff35922385a36b4048292d3e650
WildStudio <christian.aranda@gmail.com>
2019-10-14 11:03:14 +0200

    initial commit

---
+++ RandomUser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by wearemobilefirst on 14/10/2019.
+//  Copyright © 2019 adevinta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
